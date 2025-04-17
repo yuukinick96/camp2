ab930311c6382cdb00e7e356eb77b11854a31e5e
yuukinick96 <96895739+yuukinick96@users.noreply.github.com>
2025-04-17 15:39:36 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function page1(){
+	window.location.href = "page1.html"
+}
+
+function page2(){
+	window.location.href = "page2.html"
+}
+
+function page3(){
+	window.location.href = "page3.html"
+}
+
+function page4(){
+	window.location.href = "page4.html"
+}
+
+function page5(){
+	window.location.href = "page5.html"
+}
+
